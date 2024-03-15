d1931d624dede34d9864c26eeb54fc8063bdc8c2
Miranda <mirandazhu@berkeley.edu>
2024-03-14 20:40:49 -0700

    ID assigned

--- home.js
+++ home.js
@@ -1,12 +1,36 @@
 function traverse(node) {
-    if (node.nodeType == Node.TEXT_NODE) {
-      if (node.textContent.trim().length != 0) {
-        node.textContent = "Hello";
-      };
+  if (node.nodeType == Node.TEXT_NODE) {
+    if (node.textContent.trim().length != 0) {
+      node.textContent = "Hello";
     }
-    node.childNodes.forEach(child => {
-      traverse(child);
-    });
+  }
+  node.childNodes.forEach((child) => {
+    traverse(child);
+  });
 }
 
 traverse(document.body);
+
+function hashCode(str) {
+  let hash = 0;
+  if (str.length === 0) {
+    return hash;
+  }
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash |= 0;
+  }
+  return hash;
+}
+function assignid(node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    if (node.textContent.trim().length !== 0) {
+      const hashedValue = hashCode(node.textContent.trim());
+      node.setAttribute("data-hash-id", hashedValue);
+    }
+  }
+  node.childNodes.forEach((child) => {
+    assignid(child);
+  });
+}
