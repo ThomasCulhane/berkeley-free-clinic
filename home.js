f39f7bbcd515999e8716afcfc5d36f68baac84fb
Thomas Culhane <thomasjculhane@berkeley.edu>
2024-03-14 20:42:00 -0700

    translate function pt 2

--- home.js
+++ home.js
@@ -1,3 +1,18 @@
+function translate(lang) {
+  translateNode(lang, document.body)
+}
+
+function translateNode(lang, node) {
+  if (node.nodeType == Node.TEXT_NODE) {
+    if (node.textContent.trim().length != 0) {
+      node.textContent = translations[node.id][lang];
+    };
+  }
+  node.childNodes.forEach(child => {
+    translateNode(lang, child);
+  });
+}
+
 function traverse(node) {
     if (node.nodeType == Node.TEXT_NODE) {
       if (node.textContent.trim().length != 0) {
